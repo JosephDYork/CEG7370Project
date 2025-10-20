e342c02c78f28c563427b9b618656dcbd8228474
JosephDYork <York.62@Wright.edu>
2025-10-19 22:36:53 -0400

    Initial commit for project skeleton and general setup

---
+++ frontend/src/app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <main>
+      </main>
+    </div>
+  );
+};
+
+export default App;
